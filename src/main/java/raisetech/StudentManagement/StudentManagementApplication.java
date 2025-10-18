0396ae0d01d285cf8001bfc84145e5f93cf6e327
yasuko <oko3etaberr0w@gmail.com>
2025-10-18 23:33:55 +0900

    ちゃんとリストでデータが取れてくることを見るために、@RequestParam削除とSelect文を修正しました。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,11 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -26,8 +22,8 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/student")
-	public List<Student> getStudentList(@RequestParam String name){
-		return repository.searchByName(name);
+	@GetMapping("/studentList")
+	public List<Student> getStudentList(){
+		return repository.search();
 	}
 }
--- src/main/java/raisetech/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -1,15 +1,12 @@
 package raisetech.StudentManagement;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM students WHERE name = #{name}")
-  List<Student> searchByName(String name); //nameに合致するstudentsをリストで返す
+  @Select("SELECT * FROM students")
+  List<Student> search(); //nameに合致するstudentsをリストで返す
 }
