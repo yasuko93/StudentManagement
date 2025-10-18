fe21438ead8b3bea903dcffcbc5ba79b1dfc2ce1
yasuko <oko3etaberr0w@gmail.com>
2025-10-19 00:41:04 +0900

    studentsDBに合わせてStudentクラスを修正しました。

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,22 +1,12 @@
 package raisetech.StudentManagement;
 
 public class Student {
+  private int id;
   private String name;
+  private String kanaName;
+  private String nickname;
+  private String email;
+  private String area;
   private int age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String sex;
 }
