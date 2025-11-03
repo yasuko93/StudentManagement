7314107368de3e70469ed810deb4b5dbe8a53bae
yasuko <oko3etaberr0w@gmail.com>
2025-11-03 23:02:20 +0900

    dataパッケージとrepositoryパッケージを作成しました。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.Students;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentsRepository;
 
 @SpringBootApplication
 @RestController
--- src/main/java/raisetech/StudentManagement/repository/StudentsRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentsRepository.java
@@ -1,8 +1,10 @@
-package raisetech.StudentManagement;
+package raisetech.StudentManagement.repository;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import raisetech.StudentManagement.data.Students;
+import raisetech.StudentManagement.data.StudentsCourses;
 
 @Mapper
 public interface StudentsRepository {
