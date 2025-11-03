cc36792a22a6cc00679a82f2c6079077021c58b3
yasuko <oko3etaberr0w@gmail.com>
2025-11-03 23:50:54 +0900

    Serviceクラスを実装しました。

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,5 +1,30 @@
 package raisetech.StudentManagement.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import raisetech.StudentManagement.data.Students;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentsRepository;
+
+//Serviceがrepositoryを呼び出す
+
+@Service
 public class StudentService {
 
+  private StudentsRepository repository;
+
+  @Autowired
+  public StudentService(StudentsRepository repository) {
+    this.repository = repository;
+  }
+
+  //controllerに書いていたメソッドを移動（責務を分ける）
+  public List<Students> getStudentList() {
+    return repository.searchStudents();
+  }
+
+  public List<StudentsCourses> getStudentCoursesList() {
+    return repository.searchStudentsCourses();
+  }
 }
