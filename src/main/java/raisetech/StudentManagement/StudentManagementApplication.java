5cab286330359fe04bb26b6fd6d36bccfec7b48d
yasuko <oko3etaberr0w@gmail.com>
2025-12-28 01:42:40 +0900

    last commit

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,19 +3,40 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Enami Kouji";
+	private int age = 37;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name,int age){
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/updateStudentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
+
+	@PostMapping("/updateStudentAge")
+	public void updateStudentAge(int age) {
+		this.age = age;
 	}
 
 }
