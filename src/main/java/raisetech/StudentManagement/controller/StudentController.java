a5b44da21c93834b5ee754e65e590052b0ebd732
yasuko <oko3etaberr0w@gmail.com>
2026-01-13 21:16:20 +0900

    last commit

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -3,6 +3,8 @@ package raisetech.StudentManagement.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
@@ -11,7 +13,7 @@ import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
   private StudentService service;
   private StudentConverter converter;
@@ -23,12 +25,14 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList(){
+  public String getStudentList(Model model){ //①戻り値をStringに　②Springからmodel（View用の入れ物）をもらう
 
   List<Student> students = service.searchStudentList();
   List<StudentCourse> studentCourses = service.searchStudentCourseList();
 
-    return converter.convertStudentDetails(students, studentCourses);
+  //③modelにView用のデータを書き込む（「変数htmlStudentList（中身：studentDetailList）を定義します」）
+  model.addAttribute("htmlStudentList", converter.convertStudentDetails(students, studentCourses));
+  return "studentList"; //④SpringにView名（studentList.html）を返す
   }
 
 
