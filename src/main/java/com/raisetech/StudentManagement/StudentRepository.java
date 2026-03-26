b8fa5f0e6ec5a9ad8d0ea16a606097bef7c1e453
yasuko <oko3etaberr0w@gmail.com>
2026-03-26 23:18:06 +0900

    StudentRepository作成

---
+++ src/main/java/com/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.raisetech.StudentManagement;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+}
