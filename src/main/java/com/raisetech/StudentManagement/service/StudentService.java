24172c6ee003e0a197c1d5ebd8998c659923f6b8
yasuko <oko3etaberr0w@gmail.com>
2026-04-06 22:10:25 +0900

    Service層の実装

---
+++ src/main/java/com/raisetech/StudentManagement/service/StudentService.java
@@ -0,0 +1,29 @@
+package com.raisetech.StudentManagement.service;
+
+import com.raisetech.StudentManagement.data.Student;
+import com.raisetech.StudentManagement.data.StudentCourse;
+import com.raisetech.StudentManagement.repository.StudentRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentList(){
+    return repository.search();
+  }
+
+  public List<StudentCourse> searchStudentCourseList(){
+    return repository.searchCourse();
+  }
+
+
+
+}
