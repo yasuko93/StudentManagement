02ca21efa7d3712039a8406fb012dfe455cf9b86
yasuko <oko3etaberr0w@gmail.com>
2026-04-06 21:34:45 +0900

    repository用のパッケージ作成、移動

--- src/main/java/com/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/com/raisetech/StudentManagement/repository/StudentRepository.java
@@ -18,12 +18,4 @@ public interface StudentRepository {
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchCourse();
 
-  @Insert("INSERT INTO student VALUES(#{name}, #{age})")
-  void registerStudent(String name, int age);
-
-  @Update ("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudent(int age, String name);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
 }
