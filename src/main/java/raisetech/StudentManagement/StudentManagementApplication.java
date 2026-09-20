03497a5c782d9408a23748d28ba27193af95d76e
yasuko <oko3etaberr0w@gmail.com>
2026-09-20 17:35:26 +0900

    MySQLからデータを取得し、アプリ上でCRUD処理の実装

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,13 +1,45 @@
 package raisetech.StudentManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManagementApplication {
+
+	@Autowired
+	private StudentRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name){
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
+	}
+
+	@PostMapping("/student")
+	public void registerStudent(String name, int age){
+		repository.registerStudent(name, age);
+	}
+
+	@PatchMapping("/student")
+	public void updateStudent(String name, int age){
+		repository.updateStudent(name, age);
+	}
+
+	@DeleteMapping("/student")
+	public void daleteStudent(String name){
+		repository.deleteStudent(name);
 	}
 
 }
